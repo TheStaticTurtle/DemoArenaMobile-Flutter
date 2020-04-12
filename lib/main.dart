379b59d3a23317d36ac4c728af5e077c5ea8c1ab
Samuel <tuglersamuel@gmail.com>
2020-04-12 23:03:24 +0200

    Added shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:demoarenamobile_flutter_port/DemoArenaUtils.dart';
+import 'package:demoarenamobile_flutter_port/Utils.dart';
 import 'package:flutter/foundation.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -95,6 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> connect(BuildContext ctx) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
     setState(() { _image = "assets/images/info_iut.gif"; });
 
     demoarena.updateCredentials("testuser","testusepassword");
@@ -131,9 +133,158 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  final inputUserameController = TextEditingController();
+  final inputPasswordController = TextEditingController();
+  final inputINEController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    inputUserameController.dispose();
+    inputPasswordController.dispose();
+    inputINEController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final _formKey = GlobalKey<FormState>();
+    return FutureBuilder<SharedPreferences>(
+        future: SharedPreferences.getInstance(),
+        builder: (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+          Widget children;
+
+          if (snapshot.hasData) {
+            String _username = snapshot.data.getString("username") ?? "";
+            String _password = snapshot.data.getString("password") ?? "";
+            String _ine = snapshot.data.getString("ine") ?? "";
+
+            inputUserameController.text = _username;
+            inputPasswordController.text = _password;
+            inputINEController.text = _ine;
+
+            children = Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: FittedBox(
+                    child: Image.asset('$_image'),
+                    fit: BoxFit.fill,
+                  ),
+                  padding: EdgeInsets.only(top: 15.0,bottom: 30.0,left: 80.0,right: 80.0),
+                ),
+                Text(
+                  '$_statusText',
+                ),
+                Form(
+                    key: _formKey,
+                    child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 15.0,bottom: 0,left: 20.0,right: 20.0),
+                            child: Table(
+                              columnWidths: {1: FractionColumnWidth(.6)},
+                              children: [
+                                TableRow(children: [
+                                  Text( "Enter your username: " ),
+                                  TextFormField(
+                                    controller: inputUserameController,
+                                    validator: (String value) {
+                                      if(value.isEmpty) return "Shouldn't be empty";
+                                      return null;
+                                    },
+                                  ),
+                                ]),
+                                TableRow(children: [
+                                  Text( "Enter your password: " ),
+                                  TextFormField(
+                                    controller: inputPasswordController,
+                                    validator: (String value) {
+                                      if(value.isEmpty) return "Shouldn't be empty";
+                                      return null;
+                                    },
+                                  ),
+                                ]),
+                                TableRow(children: [
+                                  Text( "Enter your INE number: " ),
+                                  TextFormField(
+                                    controller: inputINEController,
+                                    validator: (String value) {
+                                      if(value.length != 8) return "Ine format invalid (len!=8)";
+                                      if(!isNumeric(value)) return "Ine format invalid (not numeric)";
+                                      if( value.contains("Infinity") || value.contains("+") || value.contains("-") ) return "Do not try to mess with me";
+                                      return null;
+                                    },
+                                  ),
+                                ]),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 15,bottom: 0,left: 20.0,right: 20.0),
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: ButtonTheme(
+                                height: 45.0,
+                                child: RaisedButton(
+                                  textColor: Colors.white,
+                                  color: Colors.redAccent,
+                                  onPressed: () => {
+                                    if (_formKey.currentState.validate()) {
+                                      snapshot.data.setString("username", inputUserameController.text),
+                                      snapshot.data.setString("password", inputPasswordController.text),
+                                      snapshot.data.setString("ine", inputINEController.text),
+                                      connect(context)
+                                    }
+                                  },
+                                  child: Text("Login"),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]
+                    )
+                )
+              ],
+            );
+
+          } else if (snapshot.hasError) {
+            renderApplicationError(snapshot.error, snapshot.error);
+          } else {
+            children = Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  child: CircularProgressIndicator(),
+                  width: 60,
+                  height: 60,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting result...'),
+                )
+              ]
+            );
+          }
+
+          return Scaffold(
+              appBar: AppBar(
+                title: Text(widget.title),
+              ),
+              body:SingleChildScrollView(
+                child: children
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () => _openUrl("https://github.com/TurtleForGaming/DemoArenaMobile/issues/new/choose"),
+                tooltip: 'Report a bug',
+                child: Icon(Icons.error),
+              ), // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        }
+    );
+/*
+    Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -151,41 +302,72 @@ class _MyHomePageState extends State<MyHomePage> {
                   Text(
                     '$_statusText',
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 15.0,bottom: 0,left: 20.0,right: 20.0),
-                    child: Table(
-                      columnWidths: {1: FractionColumnWidth(.6)},
-                      children: [
-                        TableRow(children: [
-                          Text( "Enter your username: " ),
-                          TextFormField(),
-                        ]),
-                        TableRow(children: [
-                          Text( "Enter your password: " ),
-                          TextFormField(),
-                        ]),
-                        TableRow(children: [
-                          Text( "Enter your INE number: " ),
-                          TextFormField(),
-                        ]),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 15,bottom: 0,left: 20.0,right: 20.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: ButtonTheme(
-                        height: 45.0,
-                        child: RaisedButton(
-                          textColor: Colors.white,
-                          color: Colors.redAccent,
-                          onPressed: () => connect(context),
-                          child: Text("Login"),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 15.0,bottom: 0,left: 20.0,right: 20.0),
+                          child: Table(
+                            columnWidths: {1: FractionColumnWidth(.6)},
+                            children: [
+                              TableRow(children: [
+                                Text( "Enter your username: " ),
+                                TextFormField(
+                                  initialValue: '$_username',
+                                  validator: (String value) {
+                                    if(value.isEmpty) return "Shouldn't be empty";
+                                    return null;
+                                  },
+                                ),
+                              ]),
+                              TableRow(children: [
+                                Text( "Enter your password: " ),
+                                TextFormField(
+                                  initialValue: '$_password',
+                                  validator: (String value) {
+                                    if(value.isEmpty) return "Shouldn't be empty";
+                                    return null;
+                                  },
+                                ),
+                              ]),
+                              TableRow(children: [
+                                Text( "Enter your INE number: " ),
+                                TextFormField(
+                                  initialValue: '$_ine',
+                                  validator: (String value) {
+                                    if(value.length != 8) return "Ine format invalid (len!=8)";
+                                    if(!isNumeric(value)) return "Ine format invalid (not numeric)";
+                                    if( value.contains("Infinity") || value.contains("+") || value.contains("-") ) return "Do not try to mess with me";
+                                    return null;
+                                  },
+                                ),
+                              ]),
+                            ],
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 15,bottom: 0,left: 20.0,right: 20.0),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: ButtonTheme(
+                              height: 45.0,
+                              child: RaisedButton(
+                                textColor: Colors.white,
+                                color: Colors.redAccent,
+                                onPressed: () => {
+                                  if (_formKey.currentState.validate()) {
+                                    connect(context)
+                                  }
+                                },
+                                child: Text("Login"),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]
+                    )
+                  )
               ],
             )
 
@@ -196,5 +378,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.error),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+*/
   }
 }
